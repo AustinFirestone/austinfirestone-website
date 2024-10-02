325e00d3fdc4a978ca3ac44c105861d9ea2937a6
Austin Firestone <austinfirestone@outlook.com>
2024-10-02 15:55:43 -0500

    -updates to webites

---
+++ script.js
@@ -0,0 +1,8 @@
+document.querySelector('.menu-btn').addEventListener('click', function() {
+    const menu = document.querySelector('.menu');
+    if (menu.style.display === 'block') {
+        menu.style.display = 'none';
+    } else {
+        menu.style.display = 'block';
+    }
+});
